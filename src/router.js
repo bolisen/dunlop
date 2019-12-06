618840cf021cd2b3babe4a1b5316d5de986e2a80
zhengfeng <zhengfeng@myun.info>
2019-12-06 10:42:28 +0800

    页面修改

--- src/router.js
+++ src/router.js
@@ -9,68 +9,68 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue")
     },
     {
       path: "/features",
       name: "Features",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "features" */ "./views/Features.vue")
     },
     {
       path: "/schedule",
       name: "Schedule",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "schedule" */ "./views/Schedule.vue")
     },
     {
       path: "/schedule2",
       name: "Schedule2",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "schedule2" */ "./views/Schedule2.vue")
     },
     {
       path: "/schedule3",
       name: "Schedule3",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "schedule3" */ "./views/Schedule3.vue")
     },
     {
       path: "/schedule4",
       name: "Schedule4",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "schedule4" */ "./views/Schedule4.vue")
     },
     {
       path: "/tips",
       name: "Tips",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () => import(/* webpackChunkName: "tips" */ "./views/Tips.vue")
     },
     {
       path: "/pickup",
       name: "Pickup",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "pickup" */ "./views/Pickup.vue")
     },
     {
       path: "/around",
       name: "Around",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () =>
         import(/* webpackChunkName: "around" */ "./views/Around.vue")
     },
     {
       path: "/test",
       name: "Test",
-      meta: { title: "2020年度邓禄普代理商会议" },
+      meta: { title: "2020年度邓禄普轮胎代理商会议" },
       component: () => import(/* webpackChunkName: "test" */ "./views/Test.vue")
     },
     {
